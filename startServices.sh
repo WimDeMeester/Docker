842215dfbc51f26039218c0abd7298a71879db82
Wim De Meester <wim.demeester@ster.kuleuven.be>
2016-02-04 10:41:19 +0100

    Use MariaDB 10.1 for DeepskyLog.

--- startServices.sh
+++ startServices.sh
@@ -12,18 +12,20 @@ else
 fi
 
 tail -F /var/log/httpd/error_log &
-mysql_install_db &
+chown -R mysql /var/lib/mysql
+mysql_install_db
+chown -R mysql /var/lib/mysql
 /usr/bin/mysqld_safe &
 sleep 10s
 RESULT=`mysqlshow deepskylog | grep -o deepskylog`
 if [ "$RESULT" != "deepskylog" ]; then
-  echo "CREATE database deepskylog;" | mysql 
-  mysql deepskylog < /www.deepskylog.org.sql 
-  rm /www.deepskylog.org.sql 
+  echo "CREATE database deepskylog;" | mysql
+  mysql deepskylog < /www.deepskylog.org.sql
+  rm /www.deepskylog.org.sql
 fi
 
 # Add an admin user
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'deepskylog'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 
-exec httpd -D FOREGROUND 
+exec httpd -D FOREGROUND
